5779ce01c42db0417bd0f5907feece92c4147cda
wxc <wangxiaocheng112@icloud.com>
2022-04-27 21:46:25 +0800

    update

--- src/hoc/withBreadcrumb.tsx
+++ src/hoc/withBreadcrumb.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import $store from '@/redux/index'
 import { actionCreators, actionTypes } from '@/redux/modules/settings'
 
+// 添加action后返回对应装饰器，输入组件返回高级组件
 const withBreadcrumb = (options: actionTypes.BreadcrumbData[]) => {
     return <T extends {}>(WrappedComponent: React.ComponentType<T>) => {
         return (props: T) => {
