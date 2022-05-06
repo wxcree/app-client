82b436c0dc9be989e6ae3aa41eb4960f68818571
wxc <wangxiaocheng112@icloud.com>
2022-05-06 15:22:29 +0800

    update: 图表展示增加隐藏列功能

--- src/containers/Table/index.tsx
+++ src/containers/Table/index.tsx
@@ -1,34 +1,54 @@
 import * as React from 'react'
 import withBreadcrumb from '@/hoc/withBreadcrumb'
-import { Divider, Layout } from 'antd'
-import AdminTable from '@/components/AdminTable'
+import { Layout } from 'antd'
 import { ColumnProps } from 'antd/es/table'
-import { connect } from 'react-redux'
-import { actionTypes, actionCreators } from '@/redux/modules/table'
-import { RootState } from '@/redux/Types'
-import { ThunkDispatch } from 'redux-thunk'
 import MyTree from '@/components/Collapse/Tree'
 import { getBasePkgs, getTable } from '@/services/table'
-import { getClomus } from '@/utils/table'
-import { SheetComponent } from '@antv/s2-react'
+import { SheetComponent, Switcher } from '@antv/s2-react'
 import '@antv/s2-react/dist/style.min.css'
 import { DataFrame } from '@antv/data-wizard'
 import { S2DataConfig } from '@antv/s2'
+import { SwitcherResult } from '@antv/s2-react/esm/components/switcher/interface'
+import { initSwitcherFields } from '@/utils/table'
 
-interface ITableProps {
-    /**
-     * @param F 当前页码。 eg：{ currentPage： 1 }
-     */
-    getBaseTableData: (F: object) => void
-    baseTableData: actionTypes.IBaseTableData
+function generateSwitcherFields(updatedResult: any) {
+    return {
+        columns: {
+            selectable: true,
+            items: updatedResult.columns.items
+        }
+    }
 }
 
-const TablePage: React.FunctionComponent<ITableProps> = (props) => {
+// 生成 dataCfg fields 结构
+function generateFields(updatedResult: any) {
+    return {
+        columns: updatedResult.columns.items.map((i: any) => i.id)
+    }
+}
+const defaultFields = {
+    columns: ['province', 'city', 'type', 'sub_type', 'number']
+}
+
+const defaultSwitcherFields = {
+    columns: {
+        selectable: true,
+        items: [
+            { id: 'province' },
+            { id: 'city' },
+            { id: 'type' },
+            { id: 'sub_type' },
+            { id: 'number' }
+        ]
+    }
+}
+
+const TablePage: React.FunctionComponent = (props) => {
     const [DataPkgs, setDataPkgs] = React.useState<any>([])
     const [SelectNode, setSelectNode] = React.useState<React.Key>()
 
-    const [TableData, setTableData] = React.useState<[]>([])
-    const [TableColumns, setTableColumns] = React.useState<ColumnProps<any>[]>()
+    const [hiddenColumnFields, setHiddenColumnFields] = React.useState([])
+    const [switcherFields, setSwitcherFields] = React.useState<any>()
 
     const [s2DataConfig, setS2DataConfig] = React.useState<S2DataConfig>({
         fields: {
@@ -37,7 +57,6 @@ const TablePage: React.FunctionComponent<ITableProps> = (props) => {
         data: []
     })
     React.useEffect(() => {
-        props.getBaseTableData({ currentPage: 1 })
         getBasePkgs({}).then((Response) => {
             console.log(Response)
             setDataPkgs(Response.data.data)
@@ -66,10 +85,23 @@ const TablePage: React.FunctionComponent<ITableProps> = (props) => {
             },
             data: table
         })
+        const sf = {
+            columns: {
+                selectable: true,
+                items: columns?.map((i: string) => {
+                    return { id: i, checked: true }
+                })
+            }
+        }
+        setSwitcherFields(sf)
     }
 
-    const handleChangePage = (currentPage: number) => {
-        props.getBaseTableData({ currentPage })
+    const onSubmit = (result: any) => {
+        const data = Object.assign({}, s2DataConfig)
+        data.fields = generateFields(result)
+        setS2DataConfig(data)
+        setSwitcherFields(generateSwitcherFields(result))
+        setHiddenColumnFields(result.columns.hideItems.map((i: any) => i.id))
     }
 
     const s2Options = {
@@ -77,53 +109,28 @@ const TablePage: React.FunctionComponent<ITableProps> = (props) => {
         height: 800
     }
 
-    const { baseTableData } = props
     return (
         <Layout className="inner-layout">
             <Layout.Sider style={{ width: 300, background: 0xffffff }}>
                 <MyTree datapkgs={DataPkgs} onSelect={onSelect}></MyTree>
             </Layout.Sider>
             <Layout.Content>
-                <SheetComponent dataCfg={s2DataConfig} options={s2Options} sheetType="table" />
-                <AdminTable<actionTypes.ITableData>
-                    isExport
-                    isShowPage
-                    columns={TableColumns}
-                    dataSource={TableData}
-                    // totalPage={baseTableData.totalPage}
-                    // onPageChange={handleChangePage}
+                <Switcher sheetType="table" {...switcherFields} onSubmit={onSubmit} />
+                <SheetComponent
+                    dataCfg={s2DataConfig}
+                    options={{ ...s2Options, interaction: { hiddenColumnFields } }}
+                    sheetType="table"
                 />
             </Layout.Content>
         </Layout>
     )
 }
 
-const mapStateToProps = (state: RootState) => {
-    return {
-        baseTableData: state.table.baseTableData
+export default withBreadcrumb([
+    {
+        title: '数据准备'
+    },
+    {
+        title: '数据展示'
     }
-}
-
-const mapDispatchToProps = (
-    dispatch: ThunkDispatch<actionTypes.ITableState, void, actionTypes.TableActions>
-) => {
-    return {
-        getBaseTableData: async (payload: object) => {
-            dispatch(actionCreators.tableDataAxios(payload))
-        }
-    }
-}
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(
-    withBreadcrumb([
-        {
-            title: '数据准备'
-        },
-        {
-            title: '数据展示'
-        }
-    ])(TablePage)
-)
+])(TablePage)
